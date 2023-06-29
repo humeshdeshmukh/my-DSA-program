199482e3785064d9e87f8a676f40f1d9fc967356
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2023-06-29 11:22:27 +0530

    new code

---
+++ mydirectory/prog-insertion-short01.c
@@ -0,0 +1,52 @@
+/*write a program of insertion short*/
+#include <stdio.h>
+
+void insertion(int *a, int n);
+
+int main()
+{
+    int a[10];
+    int i;
+
+    printf("\n Enter any 10 numbers");
+    for (i = 0; i < 10; i++)
+        scanf("%d", &a[i]);
+
+    printf("\n Array before sorting\n");
+    for (i = 0; i < 10; i++)
+        printf(" %d", a[i]);
+
+    insertion(a, 10);
+
+    printf("\n Array after sorting\n");
+    for (i = 0; i < 10; i++)
+        printf("\t%d", a[i]);
+
+    return 0;
+}
+
+void insertion(int *p, int n)
+{
+    int i, j, k, index, t;
+
+    for (i = 1; i < n; i++)
+    {
+        index = p[i];
+        j = i;
+
+        while ((j > 0) && (p[j - 1] > index))
+        {
+            t = p[j];
+            p[j] = p[j - 1];
+            p[j - 1] = t;
+            j--;
+        }
+
+        printf("\n");
+
+        for (k = 0; k < n; k++)
+            printf(" %d", p[k]);
+
+        printf("\n");
+    }
+}
